655dc09bceb2c6c859eff67ca88d20a45378f990
Alexa Karp <alexakarp@hotmail.com>
2022-05-09 00:07:03 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+const name = ["Charlie", "Samip", "Ali"];
+const event = "birthday";
+const arrayPrint = [];
+
+function writeCards(name, event) {
+    for (let i = 0; i < name.length; i++) {
+        arrayPrint[i]=`Thank you, ${name[i]}, for the wonderful ${event} gift!`;
+    }
+    return arrayPrint;
+}
+
+
+function countDown(number) {
+    let i = number;
+    while (i >= 0) {
+        console.log (i);
+        i--;
+    }
+}
